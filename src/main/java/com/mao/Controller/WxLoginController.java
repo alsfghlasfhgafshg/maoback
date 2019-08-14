2014d3f27ac44fdfda56528ec0a9ad3adfbefac8
dewittt <dewittt@163.com>
2019-08-14 08:59:10 +0800

    update

---
+++ src/main/java/com/mao/Controller/WxLoginController.java
@@ -0,0 +1,13 @@
+package com.mao.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class WxLoginController {
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    public void login(){
+        return;
+    }
+}
